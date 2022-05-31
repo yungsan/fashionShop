1a448536d40d8cbe86c873dcfd000f1d2dd9a410
yungsan <jjzaczac@gmail.com>
2022-05-31 12:02:21 +0700

    fix env

--- database/config.js
+++ database/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect(){
     try {
-        await mongoose.connect(process.env.LOCAL_DB);
+        await mongoose.connect(process.env.LOCAL_DB || process.env.HOST_DB);
         console.log('Connected!');
     } catch (error) {
         throw error;
