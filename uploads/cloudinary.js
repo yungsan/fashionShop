211174a247a16d0012259fa8fc5945da96de3a6f
yungsan <jjzaczac@gmail.com>
2022-06-01 15:48:00 +0700

    edit env

--- uploads/cloudinary.js
+++ uploads/cloudinary.js
@@ -3,7 +3,7 @@ const cloudinary = require('cloudinary').v2;
 cloudinary.config({ 
   cloud_name: process.env.CLOUD_NAME, 
   api_key: process.env.CLOUD_KEY, 
-  api_secret: process.env.CLOUD_API_SECRECT,
+  api_secret: process.env.CLOUD_API_SECRET,
   secure: true
 });
 
